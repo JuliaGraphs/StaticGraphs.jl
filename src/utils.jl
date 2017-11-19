896c7d76e3fa5c0ca2876ae7c2f63ad6d305c206
Seth Bromberger <github@bromberger.com>
2017-11-19 12:06:07 -0800

    fix deps

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,4 @@
-immutable UnsafeVectorView{T,U} <: AbstractVector{T}
+struct UnsafeVectorView{T,U} <: AbstractVector{T}
     offset::U
     len::U
     ptr::Ptr{T}
@@ -9,7 +9,7 @@ end
 @inline Base.getindex(v::UnsafeVectorView, idx) = unsafe_load(v.ptr, idx + v.offset)
 @inline Base.setindex!(v::UnsafeVectorView, value, idx) = unsafe_store!(v.ptr, value, idx + v.offset)
 @inline Base.length(v::UnsafeVectorView) = v.len
-Base.IndexStyle{V <: UnsafeVectorView}(::Type{V}) = IndexLinear()
+Base.IndexStyle(::Type{V}) where V <: UnsafeVectorView = IndexLinear()
 
 """
 UnsafeVectorView only works for isbits types. For other types, we're already
@@ -18,7 +18,7 @@ This function looks type-unstable, but the isbits(T) test can be evaluated
 by the compiler, so the result is actually type-stable.
 From https://github.com/rdeits/NNLS.jl/blob/0a9bf56774595b5735bc738723bd3cb94138c5bd/src/NNLS.jl#L218.
 """
-@inline function fastview{T}(parent::Union{Vector{T}, Base.FastContiguousSubArray{T}}, range::UnitRange)
+@inline function fastview(parent::Union{Vector{T}, Base.FastContiguousSubArray{T}}, range::UnitRange) where T
     if isbits(T)
         UnsafeVectorView(parent, range)
     else
