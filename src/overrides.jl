b214224d7c5b14253b18da17a49f5ae3acf2b4ed
Simone Carlo Surace <simsurace@gmx.net>
2025-08-26 12:22:26 +0200

    Fix failing test

--- src/overrides.jl
+++ src/overrides.jl
@@ -6,7 +6,7 @@ adjacency_matrix(g::StaticGraph{I,U}, T::DataType; dir = :out!) where I<:Integer
 
 function adjacency_matrix(g::StaticDiGraph{I,U}, T::DataType; dir = :out) where I<:Integer where U<:Integer
     if dir == :in
-        return SparseMatrixCSC{T,I}(nv(g), nv(g), g.f_ind, g.f_vec, ones(T, ne(g)*2))
+        return SparseMatrixCSC{T,I}(nv(g), nv(g), g.f_ind, g.f_vec, ones(T, ne(g)))
     end
     z = SparseMatrixCSC{T,I}(nv(g), nv(g), g.b_ind, g.b_vec, ones(T, ne(g)))
     dir != :out && @warn("direction `$dir` not defined for adjacency matrices on StaticGraphs; defaulting to `out`")
